f6bfce685f8f3b3e54fe7d85a23c2410c455e0d5
DESKTOP-VVPQ87N\pc <arslansahin.1907@gmail.com>
2023-08-21 18:31:41 +0300

    add product page

--- client/app/product/[slug]/page.js
+++ client/app/product/[slug]/page.js
@@ -1,7 +1,21 @@
-import React from 'react'
+'use client';
+import { useGetProductQuery } from '@/app/store/authService';
+import Breadcrumbs from '@/components/Breadcrumbs';
+import React from 'react';
 
-export default function page({params}) {
+export default function page({ params }) {
+  // fetch single product by id
+  const { data, error, isLoading, isSuccess } = useGetProductQuery(params.slug);
+
+  if (isSuccess) console.log(data);
+
+  if (isLoading) return <h1>Loading....</h1>;
+
   return (
-    <div>page: {params.slug}</div>
-  )
+    <div className="bg-gray-300">
+      <div className="container mx-auto w-full pt-6 pb-12">
+        <Breadcrumbs category={data.category} productName={data.title} />
+      </div>
+    </div>
+  );
 }
