02eb614a6cf747ba47ff47c29df27307e5e6549a
arslann <arslansahin.1907@gmail.com>
2023-09-26 18:16:10 +0300

    add footer

--- client/components/NavbarDrawer.js
+++ client/components/NavbarDrawer.js
@@ -58,7 +58,7 @@ function NavbarDrawer() {
         <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
         <div className="menu w-2/3 md:w-1/2 lg:w-1/3  h-full bg-[#333] text-white pt-10 justify-between flex-nowrap">
           {cart.length < 1 ? (
-            <h3>No products in the cart</h3>
+            <h3 className="self-center">No products in the cart</h3>
           ) : (
             <ul className="overflow-auto">
               {cart.map((product) => {
