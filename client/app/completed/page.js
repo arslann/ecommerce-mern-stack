9957595cc357903604ee1ee55849bc932ce8a615
arslann <arslansahin.1907@gmail.com>
2023-09-27 13:55:23 +0300

    fix footer

--- client/app/completed/page.js
+++ client/app/completed/page.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function page() {
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-center w-full mt-10 px-4">
+    <div className="container mx-auto ">
+      <div className="flex justify-center w-full mt-10 px-4 ">
         <p className="text-green-500 text-lg font-extrabold">
           Your order was successfully created! Thank you for shopping with us.
         </p>
