a9e1bff1ba55b035f3e9f58c3c86d36c484769de
arslann <arslansahin.1907@gmail.com>
2023-09-27 13:15:07 +0300

    update backend settings for deployment

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const connectDB = require('./database/db');
-const bodyParser = require('body-parser');
+const express = require("express");
+const connectDB = require("./database/db");
+const bodyParser = require("body-parser");
 
 // Routes import
-const products = require('./routes/api/products');
-const users = require('./routes/api/users');
-const auth = require('./routes/api/auth');
-const orders = require('./routes/api/orders');
+const products = require("./routes/api/products");
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+const orders = require("./routes/api/orders");
 
 const app = express();
 
@@ -15,24 +15,26 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-auth-token'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token"
   );
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
 
   next();
 });
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+app.get("/", (req, res) => res.send("API RUNNING"));
 
 // Routes
-app.use('/api/products', products);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/api/orders', orders);
+app.use("/api/products", products);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/orders", orders);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+export default app;
